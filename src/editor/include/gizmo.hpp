4981fe5d5df76d4df61d62e4d65d977245cfa8a8
Fxby16 <fabioscp0602@gmail.com>
2024-04-20 19:29:08 +0200

    fixed and improved the gizmo

--- src/editor/include/gizmo.hpp
+++ src/editor/include/gizmo.hpp
@@ -11,13 +11,16 @@ public:
     void SetViewPortPos(const glm::vec2 &pos){ m_ViewPortPos = pos; }
     void SetViewPortSize(const glm::vec2 &size){ m_ViewPortSize = size; }
 private:
-    void DrawGizmo(const glm::mat4 &matrix,bool vertical_arrow=false,bool horizontal_arrow=false);
+    void DrawGizmo(const glm::mat4 &matrix,bool vertical_arrow=false,bool horizontal_arrow=false,bool quad=false);
+
     bool IsMouseOnArrowH(double xpos,double ypos,const std::pair<glm::vec2,glm::vec2> &arrow);
     bool IsMouseOnArrowV(double xpos,double ypos,const std::pair<glm::vec2,glm::vec2> &arrow);
+    bool IsMouseOnQuad(double xpos,double ypos,glm::vec2 pos,glm::vec2 size);
 
     glm::vec2 m_MousePos;
     glm::vec2 m_ViewPortPos;
     glm::vec2 m_ViewPortSize;
     bool m_VerticalArrow=false;
     bool m_HorizontalArrow=false;
+    bool m_Quad=false;
 };
--- src/editor/src/gizmo.cpp
+++ src/editor/src/gizmo.cpp
@@ -6,18 +6,10 @@
 #define ARROW_WIDTH 0.05f
 #define ARROW_LENGTH 0.8f
 #define TRIANGLE_SIZE 0.1f
+#define QUAD_SIZE 0.5f
+#define QUAD_OFFSET 0.2f
 
 void Gizmo::Manipulate(glm::mat4 &matrix,const glm::mat4 &view,const glm::mat4 &projection){
-    if(glfwGetMouseButton(Window::Window,GLFW_MOUSE_BUTTON_LEFT)!=GLFW_PRESS){
-        m_MousePos.x=-1.0f;
-        m_MousePos.y=-1.0f;
-        m_VerticalArrow=false;
-        m_HorizontalArrow=false;
-
-        DrawGizmo(matrix);
-        return;   
-    }
-
     double xpos,ypos;
     glfwGetCursorPos(Window::Window,&xpos,&ypos);
 
@@ -30,14 +22,16 @@ void Gizmo::Manipulate(glm::mat4 &matrix,const glm::mat4 &view,const glm::mat4 &
 
     std::pair<glm::vec2,glm::vec2> arrow_right=std::make_pair(glm::vec2(translated_x,translated_y),glm::vec2(translated_x+ARROW_LENGTH,translated_y));
     std::pair<glm::vec2,glm::vec2> arrow_up=std::make_pair(glm::vec2(translated_x,translated_y),glm::vec2(translated_x,translated_y+ARROW_LENGTH));
+    glm::vec2 quad_pos=glm::vec2(translated_x+QUAD_OFFSET,translated_y+QUAD_OFFSET);
 
     if(!(xpos>=m_ViewPortPos.x && xpos<=m_ViewPortPos.x+m_ViewPortSize.x &&
        ypos<=m_ViewPortPos.y && ypos>=m_ViewPortPos.y-m_ViewPortSize.y)){
 
-        m_MousePos.x=-1.0f;
-        m_MousePos.y=-1.0f;
+        m_MousePos.x=std::numeric_limits<float>::min();
+        m_MousePos.y=std::numeric_limits<float>::min();
         m_VerticalArrow=false;
         m_HorizontalArrow=false;
+        m_Quad=false;
 
         DrawGizmo(matrix);
         return;
@@ -45,32 +39,43 @@ void Gizmo::Manipulate(glm::mat4 &matrix,const glm::mat4 &view,const glm::mat4 &
 
     bool vertical_arrow=IsMouseOnArrowV((xpos-m_ViewPortPos.x)/m_ViewPortSize.x*Window::MAX_WIDTH,(ypos-(m_ViewPortPos.y-m_ViewPortSize.y))/m_ViewPortSize.x*Window::MAX_WIDTH,arrow_up);
     bool horizontal_arrow=IsMouseOnArrowH((xpos-m_ViewPortPos.x)/m_ViewPortSize.x*Window::MAX_WIDTH,(ypos-(m_ViewPortPos.y-m_ViewPortSize.y))/m_ViewPortSize.x*Window::MAX_WIDTH,arrow_right);
-
-    if((!vertical_arrow && !horizontal_arrow) && m_MousePos.x==-1.0f && m_MousePos.y==-1.0f){
-        m_MousePos.x=-1.0f;
-        m_MousePos.y=-1.0f;
+    bool quad=IsMouseOnQuad((xpos-m_ViewPortPos.x)/m_ViewPortSize.x*Window::MAX_WIDTH,(ypos-(m_ViewPortPos.y-m_ViewPortSize.y))/m_ViewPortSize.x*Window::MAX_WIDTH,quad_pos,glm::vec2(QUAD_SIZE,QUAD_SIZE));
+    
+    if(glfwGetMouseButton(Window::Window,GLFW_MOUSE_BUTTON_LEFT)==GLFW_RELEASE){
+        m_MousePos.x=std::numeric_limits<float>::min();
+        m_MousePos.y=std::numeric_limits<float>::min();
         m_VerticalArrow=false;
         m_HorizontalArrow=false;
+        m_Quad=false;
 
-        DrawGizmo(matrix);
-        return;
+        DrawGizmo(matrix,vertical_arrow,horizontal_arrow,quad);
+        return;   
     }
 
-    if(!m_VerticalArrow && !m_HorizontalArrow){
+    if(!m_VerticalArrow && !m_HorizontalArrow && !m_Quad){
         m_VerticalArrow=vertical_arrow;
         m_HorizontalArrow=horizontal_arrow;
+        m_Quad=quad;
     }
 
     xpos-=m_ViewPortPos.x;
     ypos-=(Window::Height-m_ViewPortPos.y);
 
-    if(m_MousePos.x!=-1.0f && m_MousePos.y!=-1.0f){
+    if(m_MousePos.x==std::numeric_limits<float>::min() && m_MousePos.y==std::numeric_limits<float>::min()){
+        m_MousePos.x=xpos;
+        m_MousePos.y=ypos;
+    }
+
+    if(m_MousePos.x!=std::numeric_limits<float>::min() && m_MousePos.y!=std::numeric_limits<float>::min()){
         if(m_VerticalArrow){
             matrix=matrix*glm::translate(glm::mat4(1.0f),glm::vec3(0.0f,(ypos-m_MousePos.y)/m_ViewPortSize.x*Window::MAX_WIDTH,0.0f));
             DrawGizmo(matrix,true);
         }else if(m_HorizontalArrow){
             matrix=matrix*glm::translate(glm::mat4(1.0f),glm::vec3((xpos-m_MousePos.x)/m_ViewPortSize.x*Window::MAX_WIDTH,0.0f,0.0f));
             DrawGizmo(matrix,false,true);
+        }else if(m_Quad){
+            matrix=matrix*glm::translate(glm::mat4(1.0f),glm::vec3((xpos-m_MousePos.x)/m_ViewPortSize.x*Window::MAX_WIDTH,(ypos-m_MousePos.y)/m_ViewPortSize.x*Window::MAX_WIDTH,0.0f));
+            DrawGizmo(matrix,false,false,true);
         }else{
             DrawGizmo(matrix);
         }
@@ -80,7 +85,7 @@ void Gizmo::Manipulate(glm::mat4 &matrix,const glm::mat4 &view,const glm::mat4 &
     m_MousePos.y=ypos;
 }
 
-void Gizmo::DrawGizmo(const glm::mat4 &matrix,bool vertical_arrow,bool horizontal_arrow){
+void Gizmo::DrawGizmo(const glm::mat4 &matrix,bool vertical_arrow,bool horizontal_arrow,bool quad){
     glm::vec3 translation=glm::vec3(matrix[3]);
 
     float x=translation.x;
@@ -104,6 +109,12 @@ void Gizmo::DrawGizmo(const glm::mat4 &matrix,bool vertical_arrow,bool horizonta
     RENDERER->DrawTriangle(Vec2(x+ARROW_LENGTH+TRIANGLE_SIZE,y),Vec2(x+ARROW_LENGTH,y+TRIANGLE_SIZE),Vec2(x+ARROW_LENGTH,y-TRIANGLE_SIZE),horizontal_arrow_color,0);
     RENDERER->DrawTriangle(Vec2(x,y+ARROW_LENGTH+TRIANGLE_SIZE),Vec2(x+TRIANGLE_SIZE,y+ARROW_LENGTH),Vec2(x-TRIANGLE_SIZE,y+ARROW_LENGTH),vertical_arrow_color,0);
 
+    if(quad){
+        RENDERER->DrawSolidQuad(Vec2(x+QUAD_OFFSET,y+QUAD_OFFSET),Vec2(QUAD_SIZE,QUAD_SIZE),Vec4(1.0f,1.0f,1.0f,0.7f),0);   
+    }else{
+        RENDERER->DrawSolidQuad(Vec2(x+QUAD_OFFSET,y+QUAD_OFFSET),Vec2(QUAD_SIZE,QUAD_SIZE),Vec4(0.8f,0.8f,0.8f,0.7f),0);   
+    }
+
     RENDERER->Render();
 
     RENDERER->SetLineWidth(line_width);
@@ -121,6 +132,15 @@ bool Gizmo::IsMouseOnArrowH(double xpos,double ypos,const std::pair<glm::vec2,gl
 bool Gizmo::IsMouseOnArrowV(double xpos,double ypos,const std::pair<glm::vec2,glm::vec2> &arrow){
     if(xpos>=arrow.first.x-ARROW_WIDTH && xpos<=arrow.first.x+ARROW_WIDTH &&
        ypos>=arrow.first.y && ypos<=arrow.second.y){
+        return true;
+    }
+
+    return false;
+}
+
+bool Gizmo::IsMouseOnQuad(double xpos,double ypos,glm::vec2 pos,glm::vec2 size){
+    if(xpos>=pos.x && xpos<=pos.x+size.x &&
+       ypos>=pos.y && ypos<=pos.y+size.y){
         return true;
     }
 
